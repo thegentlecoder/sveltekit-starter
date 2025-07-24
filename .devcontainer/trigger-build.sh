b27ab5e58a2755b630ee7cc37805b5f1b21ab7b2
Michele Sciabarra <msciabarra@apache.org>
2025-07-24 17:21:02 +0200

    fixed devcontainer

--- .devcontainer/trigger-build.sh
+++
@@ -1,4 +0,0 @@
-TAG=$(grep 'image: sciabarracom/mastrogpt-starter' docker-compose.yml | awk -F: '{print $3}')
-git commit -m "trigger build for $TAG" -a
-git tag $TAG
-echo Ready for $TAG - run 'git  push origin main --tags'
